15fb2a57654a9ac47a2ff0f9d86fdd17ac9581a4
danielcaicedo19 <84788333+danielcaicedo19@users.noreply.github.com>
2021-09-21 12:27:34 -0500

    Add files via upload

---
+++ PROYECTO_SENA.PHP/hola.php
@@ -0,0 +1,19 @@
+<?php
+# este codigo es dificil pero no imposible:
+
+ echo "<h1>codigo php</h1>";
+
+//ya esta tarde 
+/* espero 
+que sea
+ un buen
+  trabajo
+
+*/
+
+echo "<!--comentario html-->";
+#variable 
+
+$texto =  "me gusta escribir codigo del proyecto"; 
+
+?> 
---
+++ PROYECTO_SENA.PHP/index.php
@@ -0,0 +1,59 @@
+<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>proyecto</title>
+</head>
+<body>
+<form action="validar.php" method="post">   
+<input type="submit"name="boton1"value=imprime1"> 
+<input type="submit"name="boton2"value=imprime2"> 
+<input type="submit"name="boton3"value=imprime3"> 
+<input type="submit"name="boton4"value=imprime4"> 
+
+</form>
+<iframe name"ventana"src="validar.php">
+</body>
+</html>
+
+# este codigo es dificil pero no imposible:
+
+ echo "<h1>codigo php</h1>";
+
+//ya esta tarde 
+/* espero 
+que sea
+ un buen
+  trabajo
+
+*/
+
+echo "<!--comentario html-->";
+#variable 
+
+$texto =  "me gusta escribir codigo del proyecto"; 
+
+#get
+
+echo <"hr">;
+
+$_get["nombre del proyecto"]; 
+
+
+#condiciones
+if($trabajos>=tareas){
+
+    echo "<h3>trabajos del sena</h3>";
+
+    else { "<h3>no entrego trabajos del sena</h3>";
+
+     }
+}
+
+
+
+?> 
