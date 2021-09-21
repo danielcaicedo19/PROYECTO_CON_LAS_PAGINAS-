31eee3d4a88ef834f3f63e0c3f63a7c1ee099d4a
danielcaicedo19 <84788333+danielcaicedo19@users.noreply.github.com>
2021-09-21 01:00:54 -0500

    Add files via upload

---
+++ daniel_2021/index.php
@@ -0,0 +1,52 @@
+<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>proyecto</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+# este codigo es dificil pero no imposible:
+
+ echo "<h1>codigo php</h1>";
+
+//ya esta tarde 
+/* espero 
+que sea
+ un buen
+  trabajo
+
+*/
+
+echo "<!--comentario html-->";
+#variable 
+
+$texto =  "me gusta escribir codigo del proyecto"; 
+
+#get
+
+echo <"hr">;
+
+$_get["nombre del proyecto"]; 
+
+
+#condiciones
+if($trabajos>=tareas){
+
+    echo "<h3>trabajos del sena</h3>";
+
+    else { "<h3>no entrego trabajos del sena</h3>";
+
+     }
+}
+
+
+
+?> 
